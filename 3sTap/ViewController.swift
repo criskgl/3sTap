e2dc3ad6a91bf0e0739251702401e9a7aa12f453
Cris <cris.kgl@gmail.com>
2019-01-03 17:18:47 -0800

    Initial Commit

---
+++ 3sTap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  3sTap
+//
+//  Created by CK on 03/01/2019.
+//  Copyright © 2019 CK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
